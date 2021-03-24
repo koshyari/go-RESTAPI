660fcac343a0edb5a5e4bf4b4a2d5f8f98517d43
koshyari <koshyaria@gmail.com>
2021-03-24 19:20:40 +0530

    Initialized Schema

--- main.go
+++ main.go
@@ -11,15 +11,16 @@ func initializeRouter() {
 	r := mux.NewRouter()
 
 	//API endpoints
-	r.HandleFunc("/users", GetUsers).Methods("GET")
-	r.HandleFunc("/users/{id}", GetUser).Methods("GET")
-	r.HandleFunc("/users", CreateUser).Methods("POST")
-	r.HandleFunc("/users/{id}", UpdateUser).Methods("PUT")
-	r.HandleFunc("/users/{id}", DeleteUser).Methods("DELETE")
+	r.HandleFunc("/api/users", GetUsers).Methods("GET")
+	r.HandleFunc("/api/users/{id}", GetUser).Methods("GET")
+	r.HandleFunc("/api/users", CreateUser).Methods("POST")
+	r.HandleFunc("/api/users/{id}", UpdateUser).Methods("PUT")
+	r.HandleFunc("/api/users/{id}", DeleteUser).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":9000", r))
 }
 
 func main() {
 
+	initializeRouter()
 }
